0afc92cf1cc0362d861e7f81839864095a8d88a0
yuyabu <yuyabushita@gmail.com>
2018-04-24 01:07:48 +0900

    add do method to perceptoron class

--- Perceptron.py
+++ Perceptron.py
@@ -1,6 +1,16 @@
+import numpy as np
 class Perceptron:
-	def __init__(self,w1,w2,b)
+	def __init__(self,w1,w2,b):
 		self.w1 = w1
 		self.w2 = w2
 		self.b = b
+	def do(self,x1,x2):
+		x = np.array([x1,x2])
+		w = np.array([self.w1,self.w2])
+		b = self.b
+		tmp = np.sum(w*x) + b
+		if tmp <= 0:
+			return 0
+		else:
+			return 1
 

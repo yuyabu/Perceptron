a1432f7c0b8068fa33b60ff002767a2c5f9a1fb5
yuyabu <yuyabushita@gmail.com>
2018-04-24 01:02:05 +0900

    add test about AND gate

--- test.py
+++ test.py
@@ -6,5 +6,8 @@ class TestPerceptron(unittest.TestCase):
 		self.assertEqual(perceptron.w1,0.5)
 		self.assertEqual(perceptron.w2,0.5)
 		self.assertEqual(perceptron.b,-0.7)
+	def test_and(self):
+		AND = fortest.Perceptron(0.5,0.5,-0.7)
+		self.assertEquals(1,AND.do(1,1))
 if __name__ == '__main__':
 	unittest.main()
